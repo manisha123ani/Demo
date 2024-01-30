8947b2477a7be7d3c45df995f198a20366848916
manisha kumari <manishakumari.it00@gmail.com>
2024-01-30 12:45:48 +0530

    Added new solution

---
+++ DependencyInjectionDemo/DependencyInjectionDemo/Logic/BetterDemoLogic.cs
@@ -0,0 +1,13 @@
+﻿namespace DependencyInjectionDemo.Logic;
+
+public class BetterDemoLogic
+{
+    public int Value1 { get; private set; }
+    public int Value2 { get; private set; }
+
+    public BetterDemoLogic()
+    {
+        Value1 = Random.Shared.Next(minValue: 1, maxValue: 1001);
+        Value2 = Random.Shared.Next(minValue: 1, maxValue: 1001);
+    }
+}
